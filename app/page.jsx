c55a080bb540c81a215f2e6bff6f355bd0e1c963
shamim159 <shamin_ali@hotmail.co.uk>
2024-09-26 23:19:03 +0100

    Added temp photo and some nav and mobile nav functionality

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,9 @@
 import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
 
-import React from "react";
+//components
+import Social from "@/components/Social";
+import Photo from "@/components/Photo";
 
 const Home = () => {
   return (
@@ -9,26 +11,38 @@ const Home = () => {
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/* text */}
-          <div className="text-center xl:text-left">
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Developer</span>
             <h1 className="h1">
               Hello I'm <br /> <span className="text-accent">Sam Ali</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
-              I'm a skilled web developer with a passion for crafting elegant
+              I'm a skilled developer with a passion for crafting elegant
               digital experiences and i am proficient in various programming
               languages and technologies.
             </p>
             {/* btn and socials */}
-            <div>
-              <Button>
-                m<span>Download CV</span>
-                <FiDownload />
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <Button
+                variant="outline"
+                size="lg"
+                className="uppercase flex items-center gap-2"
+              >
+                <span>Download CV</span>
+                <FiDownload className="text-xl" />
               </Button>
+              <div className="mb-8 xl:mb-0">
+                <Social
+                  containerStyles="flex gap-6"
+                  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500  "
+                />
+              </div>
             </div>
           </div>
           {/* photo */}
-          <div>photo</div>
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+            <Photo />
+          </div>
         </div>
       </div>
     </section>
---
+++ components/Social.jsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+
+import {
+  FaGithub,
+  FaInstagram,
+  FaLinkedinIn,
+  FaTiktok,
+  FaTwitter,
+  FaYoutube,
+} from "react-icons/fa";
+
+const socials = [
+  { icon: <FaGithub />, path: "" },
+  { icon: <FaLinkedinIn />, path: "" },
+  { icon: <FaYoutube />, path: "" },
+  { icon: <FaTwitter />, path: "" },
+  { icon: <FaInstagram />, path: "" },
+  { icon: <FaTiktok />, path: "" },
+];
+
+const Social = ({ containerStyles, iconStyles }) => {
+  return (
+    <div className={containerStyles}>
+      {socials.map((item, index) => {
+        return (
+          <Link key={index} href={item.path} className={iconStyles}>
+            {item.icon}
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Social;
